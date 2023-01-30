03c806dba1362c36f57b9670d3c66a5ee0ac78f4
Christian Gunderman <chgund@microsoft.com>
2023-01-30 09:29:53 -0800

    FIXUP: switching machines.

--- src/lsif-debug/NaiveDiffCommand.cs
+++ src/lsif-debug/NaiveDiffCommand.cs
@@ -28,6 +28,35 @@ namespace lsif_debug
 
             var lines = await File.ReadAllLinesAsync(lsifPath, CancellationToken.None);
 
+            var verticiesById = new Dictionary<int, JsonNode>();
+            var edgesInById = new Dictionary<int, JsonNode>();
+            var edgesOutById = new Dictionary<int, JsonNode>();
+
+            // Create a lookup table for nodes + edges.
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                var node = JsonNode.Parse(line);
+
+                if (node is not null &&
+                    (node["type"]?.AsValue().TryGetValue<string>(out var type) ?? false))
+                {
+                    if (type == "vertex" &&
+                        node["id"] is JsonValue value &&
+                        value.TryGetValue<int>(out var intValue))
+                    {
+                        verticiesById.Add(intValue, node);
+                    }
+                    else if (type == "edge")
+                    {
+                        if (node["inV"] is not null)
+                        {
+                            edgesInById.Add()
+                        }
+                    }
+                }
+            }
+
             for (int i = 0; i < lines.Length; i++)
             {
                 var line = lines[i];
